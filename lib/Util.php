54b959ca022d0282a8b102fa9653881d93aec2b6
rentingting <rentingting@zhongan.com>
2017-11-20 15:29:26 +0800

    classmap

---
+++ lib/Util.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: tingting.ren
+ * Date: 20/11/2017
+ * Time: 3:25 PM
+ */
+class Util
+{
+    public static function test()
+    {
+        echo 'Util test';
+        echo "\n";
+    }
+}
